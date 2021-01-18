1af2979e03e2666040a5b0fe545d1049f445b351
Levishemtov1 <55457708+Levishemtov1@users.noreply.github.com>
2021-01-18 15:12:55 -0500

    now have relative routes

--- client/src/components/create-exercise.component.js
+++ client/src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercises extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:5000/users/')
+        axios.get('/users/')
             .then(response => {
                 if (response.data.length > 0) {
                     this.setState({
@@ -71,7 +71,7 @@ export default class CreateExercises extends Component {
 
         console.log(exercise); 
 
-        axios.post('http://localhost:5000/exercises/add', exercise)
+        axios.post('/exercises/add', exercise)
             .then(res => console.log(res.data));
 
         window.location = '/';
